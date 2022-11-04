717a67712ef7968165cf799e041ee27a49bd9ba5
Allan Jardine <allan@datatables.net>
2022-11-04 14:51:00 +0000

    Missing files in DataTables-DataTables

--- js/autoFill.semanticui.js
+++ js/autoFill.semanticui.js
@@ -6,7 +6,7 @@
 (function( factory ){
 	if ( typeof define === 'function' && define.amd ) {
 		// AMD
-		define( ['jquery', 'datatables.net-se', 'datatables.net-autofill'], function ( $ ) {
+		define( [''], function ( $ ) {
 			return factory( $, window, document );
 		} );
 	}
@@ -19,18 +19,8 @@
 				root = window;
 			}
 
-			if ( ! $ ) {
-				$ = typeof window !== 'undefined' ? // jQuery's factory checks for a global window
-					require('jquery') :
-					require('jquery')( root );
-			}
-
 			if ( ! $.fn.dataTable ) {
-				require('datatables.net-se')(root, $);
-			}
-
-			if ( ! $.fn.dataTable.AutoFill ) {
-				require('datatables.net-autofill')(root, $);
+				require('')(root, $);
 			}
 
 
